09e52a976059d150a27410f3ef4ad84c6934c26c
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2024-10-20 20:14:52 -0300

    feat(propety-list-value-protocol): add property types

--- MoneyManagement App/Utility/UserDefault/PropertyListValueProtocol.swift
+++ MoneyManagement App/Utility/UserDefault/PropertyListValueProtocol.swift
@@ -1,7 +1,16 @@
-//
-//  Pro.swift
-//  MoneyManagement App
-//
-//  Created by Rafael Riki Ogawa Osiro on 20/10/24.
-//
+import Foundation
 
+protocol PropertyListValue {}
+
+extension Data: PropertyListValue {}
+extension String: PropertyListValue {}
+extension Date: PropertyListValue {}
+extension Bool: PropertyListValue {}
+extension Int: PropertyListValue {}
+extension Double: PropertyListValue {}
+extension Float: PropertyListValue {}
+extension UUID: PropertyListValue {}
+extension String?: PropertyListValue {}
+
+extension Array: PropertyListValue where Element: PropertyListValue {}
+extension Dictionary: PropertyListValue where Key == String, Value: PropertyListValue {}

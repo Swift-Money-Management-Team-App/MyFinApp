c43b9e4f0477668bb542b75c188a5f01edeacf5d
Raquel Santos <raquel.pri@hotmail.com>
2024-10-24 20:15:13 -0300

    feat: adds animation to splash screen

---
+++ MoneyManagement App/View/SplashScreen/SplashView.swift
@@ -0,0 +1,58 @@
+//
+//  SplashViewController.swift
+//  MoneyManagement App
+//
+//  Created by Raquel on 19/10/24.
+//
+
+import Foundation
+
+import SwiftUI
+import Lottie
+
+struct SplashView: View {
+    @State private var isActive = false
+    @State private var animationOpacity = 0.0
+
+    var body: some View {
+        ZStack {
+            Color.clear
+                .edgesIgnoringSafeArea(.all)
+
+            LottieView(animation: .named("SplashScreenAnimation.json"))
+                .playbackMode(.playing(.toProgress(1, loopMode: .playOnce)))
+                .opacity(animationOpacity)
+                .onAppear {
+                    withAnimation(.easeInOut(duration: 3.0)) {
+                        animationOpacity = 1.0
+                    }
+
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+                        withAnimation(.easeInOut(duration: 2.0)) {
+                            animationOpacity = 0.0
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            isActive = true
+                        }
+                    }
+                }
+        }
+        .fullScreenCover(isPresented: $isActive) {
+            MainView()
+        }
+    }
+}
+
+struct MainView: View {
+    var body: some View {
+        Text("Main Screen")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundColor(.black)
+    }
+}
+
+#Preview {
+    SplashView()
+}
+

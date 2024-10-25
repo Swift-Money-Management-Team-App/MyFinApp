8a9cb4612062b81261efa8d1ea8828114009c1c5
Raquel Santos <raquel.pri@hotmail.com>
2024-10-24 23:52:54 -0300

    feat: update main to handle SplashScreen and Onboarding flow

--- MoneyManagement App/MoneyManagement_AppApp.swift
+++ MoneyManagement App/MoneyManagement_AppApp.swift
@@ -3,16 +3,10 @@ import SwiftUI
 @main
 struct MoneyManagement_AppApp: App {
     let persistenceController = PersistenceController.shared
-    @AppStorage("firstLaunchApplication") var  firstLaunchApplication: Bool = Storage.share.firstLaunchApplication
-    
+
     var body: some Scene {
         WindowGroup {
-            if firstLaunchApplication {
-                OnboardingView(isFirstLaunch: $firstLaunchApplication)
-            } else {
-                ContentView()
-                    .environment(\.managedObjectContext, persistenceController.container.viewContext)
-            }
+            SplashView()
         }
     }
 }

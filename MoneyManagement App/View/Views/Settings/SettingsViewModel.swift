0637484b81e902de06d36c6262c755fbde94a265
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2024-10-28 01:18:22 -0300

    style(settings-settings-view-model): add todo comment

--- MoneyManagement App/View/Views/Settings/SettingsViewModel.swift
+++ MoneyManagement App/View/Views/Settings/SettingsViewModel.swift
@@ -7,8 +7,8 @@ class SettingsViewModel: ObservableObject {
     
     // CONSTANTS
     private let SHARED_LINK = URL(string: "https://apps.apple.com/us/app/MoneyManagementApp/id11111111")!
-    private let PRIVACY_POLICY = URL(string: "https://www.google.com.br")!
-    private let USE_TERMS = URL(string: "https://www.google.com.br")!
+    private let PRIVACY_POLICY = URL(string: "https://www.google.com.br")! // TODO: UPDATE THIS URL TO PRIVACY POLICY
+    private let USE_TERMS = URL(string: "https://www.google.com.br")! // TODO: UPDATE THIS URL TO PRIVACY POLICY
 }
 
 

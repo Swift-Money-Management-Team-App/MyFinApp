7795b886467b7436cc8814070347b01a28c94e7a
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2024-10-28 01:11:22 -0300

    refactor(shared-toggle-row): add color as param

--- MoneyManagement App/View/Components/Rows/ToggleRow.swift
+++ MoneyManagement App/View/Components/Rows/ToggleRow.swift
@@ -5,11 +5,18 @@ struct ToggleRow: View {
     @Binding var toogleValue: Bool
     var image: String
     var label: String
+    var labelForegroundStyle: Color?
+    var imageForegroundStyle: Color?
     
     var body: some View {
         Toggle(isOn: self.$toogleValue) {
-            Label(self.label, systemImage: self.image)
-                .foregroundStyle(.black)
+            Label {
+                Text(LocalizedStringResource(stringLiteral: self.label))
+                    .foregroundStyle(self.labelForegroundStyle ?? .black)
+            } icon: {
+                Image(systemName:self.image)
+                    .foregroundStyle(self.imageForegroundStyle ?? .black)
+            }
         }
     }
 }

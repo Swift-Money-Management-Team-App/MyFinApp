82d180f22c99da5fe3f533f839e2396f3010c0b2
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2024-10-28 01:10:23 -0300

    feat(shared-shared-link-row): add shared link row view builder

---
+++ MoneyManagement App/View/Components/Rows/SharedLinkRow.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct SharedLinkRow {
+    
+    @ViewBuilder
+    func createSharedLinkRow(url: URL, label: String, labelForegroundStyle: Color, isShowDisclosureIcon: Bool = false, iconSystemName: String, iconForegroundStyle: Color) -> some View {
+        
+        ShareLink(item: url) {
+            Label {
+                HStack {
+                    Text(LocalizedStringResource(stringLiteral: label))
+                        .foregroundStyle(labelForegroundStyle)
+                    
+                    if isShowDisclosureIcon {
+                        createDisclosureIcon()
+                    } else {
+                        EmptyView()
+                    }
+                }
+            } icon: {
+                Image(systemName: iconSystemName)
+                    .foregroundStyle(iconForegroundStyle)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func createDisclosureIcon(fontSize: CGFloat? = 14, foregroundStyle: Color? = .black) -> some View {
+        Spacer()
+        Image(systemName: "chevron.right")
+            .font(.system(size: 14, weight: .bold))
+            .foregroundStyle(.disclosure)
+    }
+}

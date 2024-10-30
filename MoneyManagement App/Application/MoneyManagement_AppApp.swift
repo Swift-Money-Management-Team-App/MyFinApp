3c0e6b56ec0478bd9fcc8f907eacd92625a437e3
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2024-10-30 20:59:44 -0300

    fix: changes that didn't came when merged with develop

--- MoneyManagement App/Application/MoneyManagement_AppApp.swift
+++ MoneyManagement App/Application/MoneyManagement_AppApp.swift
@@ -7,23 +7,29 @@ struct MoneyManagement_AppApp: App {
     
     let settingsVM = SettingsViewModel()
     
-    let persistenceController = PersistenceController.shared
-
-    @AppStorage("hasLaunchedBefore") private var firstLaunchApplication: Bool = true
+    @State var firstLaunchApplication: Bool = Storage.share.firstLaunchApplication
     @State private var showSplash = true
-
+    
+    let container: ModelContainer
+    
+    init() {
+        do {
+            container = try ModelContainer(for: User.self, BankAccount.self)
+        } catch {
+            fatalError("Failed to create ModelContainer.")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ZStack {
-                if firstLaunchApplication {
-                    OnboardingView(isFirstLaunch: $firstLaunchApplication)
-                        .transition(.opacity)
-                } else {
-                    ContentView()
-                        .environment(\.managedObjectContext, persistenceController.container.viewContext)
-                        .environmentObject(self.settingsVM)
-                        .transition(.opacity)
-                }
+            if firstLaunchApplication {
+                OnboardingView(isFirstLaunch: $firstLaunchApplication)
+                    .transition(.opacity)
+            } else {
+                HomeView(modelContext: container.mainContext)
+                    .modelContainer(container)
+                    .environmentObject(self.settingsVM)
+                    .transition(.opacity)
             }
         }
     }

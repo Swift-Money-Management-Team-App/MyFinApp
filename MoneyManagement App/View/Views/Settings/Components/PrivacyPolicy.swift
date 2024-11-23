df5fb88911a354e05def6af874232cc6d446ebe0
Raquel Santos <raquel.pri@hotmail.com>
2024-11-23 10:53:33 -0300

    refactor: changes privacy policy file to use localized strings

--- MoneyManagement App/View/Views/Settings/Components/PrivacyPolicy.swift
+++ MoneyManagement App/View/Views/Settings/Components/PrivacyPolicy.swift
@@ -1,91 +1,82 @@
 import SwiftUI
 
 struct PrivacyPolicy: View {
-    
-    private let titleText = TitleText()
-    private let bodyText = BodyText()
-    
     var body: some View {
         VStack(spacing: 0) {
             RoundedRectangle(cornerRadius: 20)
                 .foregroundStyle(.brightGold)
                 .frame(maxHeight: 175)
-            
-            ScrollView {
-                
-                VStack(alignment: .leading) {
-                    titleText.createTitleText("Privacy Policy")
-                    bodyText.createBodyText("""
-This privacy policy applies to the MONEY APP MANAGEMENT app (hereby referred to as "Application") for mobile devices that was created by Money Management App (hereby referred to as "Service Provider") as a Free service. This service is intended for use "AS IS".
-""")
-                    
-                    titleText.createTitleText("What information does the Application obtain and how is it used?")
-                    Text("""
-The Application does not obtain any information when you download and use it. Registration is not required to use the Application.
-""")
-                    titleText.createTitleText("Does the Application collect precise real time location information of the device?")
-                    Text("""
-This Application does not collect precise information about the location of your mobile device.
-""")
-                    
-                    titleText.createTitleText("Do third parties see and/or have access to information obtained by the Application?")
-                    Text("""
-Since the Application does not collect any information, no data is shared with third parties.
-""")
-                    
-                    titleText.createTitleText("What are my opt-out rights?")
-                    Text("""
-You can stop all collection of information by the Application easily by uninstalling it. You may use the standard uninstall processes as may be available as part of your mobile device or via the mobile application marketplace or network.
-""")
-                    
-                    titleText.createTitleText("Children")
-                        .fontWeight(.bold)
-                    Text("""
-The Application is not used to knowingly solicit data from or market to children under the age of 13.
 
-The Service Provider does not knowingly collect personally identifiable information from children. The Service Provider encourages all children to never submit any personally identifiable information through the Application and/or Services. The Service Provider encourage parents and legal guardians to monitor their children's Internet usage and to help enforce this Policy by instructing their children never to provide personally identifiable information through the Application and/or Services without their permission. If you have reason to believe that a child has provided personally identifiable information to the Service Provider through the Application and/or Services, please contact the Service Provider (contact@moneyapp.com) so that they will be able to take the necessary actions. You must also be at least 16 years of age to consent to the processing of your personally identifiable information in your country (in some countries we may allow your parent or guardian to do so on your behalf).
-""")
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    Text(LocalizedStringKey.privacyPolicyTitle.label)
+                        .font(.title)
+                        .bold()
                     
-                    titleText.createTitleText("Security")
-                    Text("""
-The Service Provider is concerned about safeguarding the confidentiality of your information. However, since the Application does not collect any information, there is no risk of your data being accessed by unauthorized individuals.
-""")
+                    Text(LocalizedStringKey.privacyPolicyDescription.message)
                     
-                    titleText.createTitleText("Changes")
-                    Text("""
-This Privacy Policy may be updated from time to time for any reason. The Service Provider will notify you of any changes to their Privacy Policy by updating this page with the new Privacy Policy. You are advised to consult this Privacy Policy regularly for any changes, as continued use is deemed approval of all changes.
-This privacy policy is effective as of 2024-12-10
-""")
+                    Text(LocalizedStringKey.informationCollectionTitle.label)
+                        .font(.headline)
                     
-                    titleText.createTitleText("Your Consent")
-                        .fontWeight(.bold)
-                    Text("""
-By using the Application, you are consenting to the processing of your information as set forth in this Privacy Policy now and as amended by the Service Provider.
-""")
+                    Text(LocalizedStringKey.informationCollectionDescription.message)
                     
-                    titleText.createTitleText("Contact Us")
-                        .fontWeight(.bold)
-                    Text("""
-If you have any questions regarding privacy while using the Application, or have questions about the practices, please contact the Service Provider via email at contact@moneyapp.com.
-""")
+                    Text(LocalizedStringKey.locationInfoTitle.label)
+                        .font(.headline)
+                    
+                    Text(LocalizedStringKey.locationInfoDescription.message)
+                    
+                    Text(LocalizedStringKey.thirdPartiesAccessTitle.label)
+                        .font(.headline)
+                    
+                    Text(LocalizedStringKey.thirdPartiesAccessDescription.message)
+                    
+                    Text(LocalizedStringKey.optOutRightsTitle.label)
+                        .font(.headline)
+                    
+                    Text(LocalizedStringKey.optOutRightsDescription.message)
+                    
+                    Text(LocalizedStringKey.childrenTitle.label)
+                        .font(.headline)
+                    
+                    Text(LocalizedStringKey.childrenDescription.message)
+                    
+                    Text(LocalizedStringKey.securityTitle.label)
+                        .font(.headline)
+                    
+                    Text(LocalizedStringKey.securityDescription.message)
+                    
+                    Text(LocalizedStringKey.changesTitle.label)
+                        .font(.headline)
+                    
+                    Text(LocalizedStringKey.changesDescription.message)
+                    
+                    Text(LocalizedStringKey.consentTitle.label)
+                        .font(.headline)
+                    
+                    Text(LocalizedStringKey.consentDescription.message)
+                    
+                    Text(LocalizedStringKey.contactUsTitle.label)
+                        .font(.headline)
+                    
+                    Text(LocalizedStringKey.contactUsDescription.message)
                 }
+                .padding()
             }
-            .padding()
         }
         .ignoresSafeArea()
-        .navigationTitle(LocalizedStringKey(stringLiteral: "Política de Privacidade"))
+        .navigationTitle(LocalizedStringKey.privacyPolicyScreenTitle.label)
         .navigationBarTitleDisplayMode(.large)
         .navigationBarBackButtonHidden(true)
-        .toolbar(content: {
+        .toolbar {
             ToolbarItem(placement: .cancellationAction) {
                 Button(action: { Navigation.navigation.screens.removeLast() }) {
                     HStack {
                         Image(systemName: "chevron.backward")
-                        Text("Configurações")
+                        Text(LocalizedStringKey.settingsButtonBack.button)
                     }
                 }
             }
-        })
+        }
     }
 }
 
